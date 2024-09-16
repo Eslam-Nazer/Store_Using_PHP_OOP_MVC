3cdb4be53ec7b65b92f988a4249fcd51f61ce19b
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-09-16 23:39:36 +0300

    add notification functions (seen and seenChecker)

--- app/template/wrapperstart.php
+++ app/template/wrapperstart.php
@@ -1,7 +1,6 @@
 <div <?= $this->session->lang == 'en' ? 'dir="ltr"' : 'dir="rtl"' ?> class="p-2 w-4/5 ltr:float-end transition translate-x-0 duration-[0.6s] flex" id="content">
     <div class="p-1 mt-8 block ml-2 w-full text-lg <?= $_SESSION['lang'] == 'ar' ? 'font-zain' : 'font-inter' ?>">
 
-
         <?php $messages = $this->messenger->getMessages();
         if (!empty($messages)) : foreach ($messages as $message) : ?>
 
