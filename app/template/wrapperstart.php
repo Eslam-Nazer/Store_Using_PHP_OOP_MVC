34b982a66f7e418c48dd43a78a93fa6c8e3b7361
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-09-23 11:45:36 +0300

    Updating content container to be more flexible

--- app/template/wrapperstart.php
+++ app/template/wrapperstart.php
@@ -1,12 +1,12 @@
-<div <?= $this->session->lang == 'en' ? 'dir="ltr"' : 'dir="rtl"' ?> class="p-2 w-4/5 ltr:float-end transition translate-x-0 duration-[0.6s] flex" id="content">
-    <div class="p-1 mt-8 block ml-2 w-full text-lg <?= $_SESSION['lang'] == 'ar' ? 'font-zain' : 'font-inter' ?>">
+        <div class="main-container">
+            <div class="content-container">
 
-        <?php $messages = $this->messenger->getMessages();
-        if (!empty($messages)) : foreach ($messages as $message) : ?>
+                <?php $messages = $this->messenger->getMessages();
+                if (!empty($messages)) : foreach ($messages as $message) : ?>
 
-                <div class="message m<?= $message[1] ?>">
-                    <p class=" mx-3"><?= $message[0] ?></p>
-                </div>
+                        <div class="message m<?= $message[1] ?>">
+                            <p class=" mx-3"><?= $message[0] ?></p>
+                        </div>
 
-        <?php endforeach;
-        endif; ?>
+                <?php endforeach;
+                endif; ?>
