0f74a0c532e361a1b5bf35dd097df677c965a327
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-09-23 12:11:13 +0300

    Type of expenses section arabic language files

---
+++ app/languages/ar/typeofexpenses/default.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$_['title'] = 'أنواع المصروفات';
+$_['text_header'] = 'أنواع المصروفات';
+$_['text_new_type_of_expenses'] = 'اضافة نوع مصروف جديد';
+$_['text_table_expenses_name'] = 'اسم المصروف';
+$_['text_table_expenses_fixed_payment'] = 'القيمه الثابته للمصروف';
+$_['text_table_control'] = 'التحكم';
+$_['text_delete_type_of_expenses'] = 'هل تود حذف هذا النوع من المصروفات';
---
+++ app/languages/ar/typeofexpenses/labels.lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$_['text_label_ExpenseName'] = 'اسم نوع المصروف باللغة الانجليزية';
+$_['text_label_ExpenseNameAr'] = 'اسم نوع المصروف باللغة العربية';
+$_['text_label_FixedPayment'] = 'قيمة الدفع الثابته';
