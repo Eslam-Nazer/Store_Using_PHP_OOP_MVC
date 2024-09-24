3262f01cfc307c687ba0a1e6f2fc98ed715684fa
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-09-24 20:25:34 +0300

    Language Arabic files for the daily expenses section

---
+++ app/languages/ar/dailyexpenses/default.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['title'] = 'المصروفات اليومية';
+$_['text_header'] = 'المصروفات اليومية';
+$_['text_new_daily_expense'] = 'مصروف يومي جديد';
+$_['text_table_expense_name'] = 'اسم المصروف';
+$_['text_table_expense_payment'] = 'قيمة دفع المصروف';
+$_['text_table_user_expense_creator'] = 'اسم من ضاف المصروف';
+$_['text_table_expense_date'] = 'تاريخ اضافة المصروف';
+$_['text_table_control'] = 'التحكم';
+$_['text_delete_daily_expenses_payment'] = 'هل تود في حذف هذا المصروف اليومي';
---
+++ app/languages/ar/dailyexpenses/messages.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['text_message_save'] = 'تم حفظ المصروف اليومي بنجاح';
+$_['text_message_error'] = 'خطأ في حفظ المصروف اليومي';
+$_['text_message_edit_success'] = 'تم التعديل علي المصروف اليومي بنجاح';
+$_['text_message_edit_error'] = 'خطأ في حفظ التعديلات علي المصروف اليومي';
+$_['text_message_delete_success'] = 'تم حذف المصروف اليومي بنجاح';
+$_['text_message_delete_error'] = 'خطأ في حذف المصروف اليومي';
