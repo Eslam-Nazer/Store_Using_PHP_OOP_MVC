6795ed8b4634e0b2a49a7e231fc38af28097ad65
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-03 22:51:32 +0300

    Add get all function and add inner join with author tables

--- app/models/purchasesinvoicesmodel.php
+++ app/models/purchasesinvoicesmodel.php
@@ -6,7 +6,7 @@ class PurchasesInvoicesModel extends AbstractModel
 {
     private $InvoiceId;
     private $SupplierId;
-    private $PaymentType;
+    private $PaymentTypeId;
     private $PaymentStatus;
     private $Created;
     private $Discount;
@@ -15,13 +15,13 @@ class PurchasesInvoicesModel extends AbstractModel
     protected static $tableName     = "app_purchases_invoices";
     protected static $primaryKey    = "InvoiceId";
     protected static $tableSchema   = [
-        "InvoiceId"     => self::DATA_TYPE_INT,
-        "SupplierId"    => self::DATA_TYPE_INT,
-        "PaymentType"   => self::DATA_TYPE_INT,
-        "PaymentStatus" => self::DATA_TYPE_INT,
-        "Created"       => self::DATA_TYPE_STR,
-        "Discount"      => self::DATA_TYPE_DECIMAL,
-        "UserId"        => self::DATA_TYPE_INT,
+        "InvoiceId"         => self::DATA_TYPE_INT,
+        "SupplierId"        => self::DATA_TYPE_INT,
+        "PaymentTypeId"     => self::DATA_TYPE_INT,
+        "PaymentStatus"     => self::DATA_TYPE_INT,
+        "Created"           => self::DATA_TYPE_STR,
+        "Discount"          => self::DATA_TYPE_DECIMAL,
+        "UserId"            => self::DATA_TYPE_INT,
     ];
 
     public function __construct() {}
@@ -34,5 +34,15 @@ class PurchasesInvoicesModel extends AbstractModel
     public function __get($prop)
     {
         return $this->$prop;
+    }
+
+    public static function getAllPurchases()
+    {
+        $sql  = "SELECT api.* , as.Name SupplierName, as.NameAr SupplierNameAr, au.Username UserCreator, apipt.*, apir.PaymentAmount FROM " . self::getModelTableName() . " api ";
+        $sql .= " INNER JOIN app_suppliers `as` ON as.SupplierId = api.SupplierId ";
+        $sql .= " INNER JOIN app_users `au` ON au.UserId = api.UserId ";
+        $sql .= " INNER JOIN app_purchases_invoices_payment_type `apipt` ON apipt.PaymentTypeId = api.PaymentTypeId ";
+        $sql .= " INNER JOIN app_purchases_invoices_receipts `apir` ON apir.InvoiceId = api.InvoiceId";
+        return self::get($sql);
     }
 }
